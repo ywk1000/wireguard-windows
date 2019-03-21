c7a781c42f8a4a24f08cca6410890a60891f797b
Anthony Dong <adongy@users.noreply.github.com>
2019-03-21 20:54:46 +0100

    Start the mock server

--- ui/tests/ui.go
+++ ui/tests/ui.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"golang.zx2c4.com/wireguard/windows/service"
 	"golang.zx2c4.com/wireguard/windows/ui"
 )
 
 func main() {
+	service.InitializeIPCClient(nil, nil, nil)
 	ui.RunUI()
 }
